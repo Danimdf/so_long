d9474aeaae85f8123ec24f74c3f3451ead38f1ec
Daniela Monteiro <danimdf360@gmail.com>
2021-11-14 14:06:15 -0300

    Errors check ok

--- source/bye.c
+++ source/bye.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 21:05:47 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/11/12 20:26:59 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/11/14 14:03:32 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,12 @@
 int bye(int key, t_game *game)
 {
 	if (key == 'd' && game->map[game->img.position.row_pos][game->img.position.col_pos + 1] == 'E')
-	{
-		if (game->obj_map.obj_c != 0)
-			printf("Please, collect all diamonds!!!!");
-		else
-			mlx_close(game);
-	}
+		mlx_close(game);
 	else if (key == 'a' && game->map[game->img.position.row_pos][game->img.position.col_pos - 1] == 'E')
-	{
-		if (game->obj_map.obj_c != 0)
-			printf("Please, collect all diamonds!!!!");
-		else
-			mlx_close(game);
-	}
+		mlx_close(game);
 	else if (key == 's' && game->map[game->img.position.row_pos + 1][game->img.position.col_pos] == 'E')
-	{
-		if (game->obj_map.obj_c != 0)
-			printf("Please, collect all diamonds!!!!");
-		else
-			mlx_close(game);
-	}
+		mlx_close(game);
 	else if (key == 'w' && game->map[game->img.position.row_pos - 1][game->img.position.col_pos] == 'E')
-	{
-		if (game->obj_map.obj_c != 0)
-			printf("Please, collect all diamonds!!!!");
-		else
-			mlx_close(game);
-	}
+		mlx_close(game);
 	return (0);
 }
